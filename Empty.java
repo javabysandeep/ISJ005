2738f13ef6400ed53b1eaeb3f7fe8c8b0d362c66
sandeep bhingle <sandeep.bhingle@itshaala.com>
2023-08-16 15:51:42 +0530

    hello world

---
+++ Empty.java
@@ -0,0 +1,7 @@
+public class Empty {
+
+
+}
+class A {}
+class B {}
+class C {}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
 public class HelloWorld {
-	public static void main(String[] args){
-		System.out.println("Hello  World");
-		
+
+	public static void main(String[] arguments){
+
+		printHello();
 	}
+
+	public static void printHello(){
+		
+		System.out.println("Hello World");
+
+	}
+
+
 }
